5b0db40f2b6319714a8a83266532ed73b8816f33
Skyboy <cacou@cc.com>
2019-09-16 17:18:24 -0400

    fin cours 7

---
+++ Cours-7/Assets/Script/Bullet.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class Bullet : MonoBehaviour {
+
+    Rigidbody rbd;
+    public int lifeSpan = 3;
+    public int bulletSpeed = 1;
+    public int BulletDamage = 1;
+    private float TimeLeafToLive;
+
+    // Use this for initialization
+    void Start () {
+        rbd = GetComponent<Rigidbody>();
+        TimeLeafToLive = lifeSpan;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        rbd.MovePosition(rbd.position + transform.forward * bulletSpeed * Time.deltaTime);
+        ApplyLifeSpan();
+
+	}
+
+    private void ApplyLifeSpan()
+    {
+        TimeLeafToLive -= Time.deltaTime;
+        if (TimeLeafToLive <0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    public void OnTriggerEnter(Collider otherObject)
+    {
+        Damagable damagable = otherObject.GetComponentInParent<Damagable>();
+
+        if (damagable != null)
+        {
+            damagable.TakeDamage(BulletDamage);
+        }
+        Die();
+    }
+}
